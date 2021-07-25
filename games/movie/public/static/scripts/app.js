0a6ac0a4d0752550645d2376b4eb5a8a63e32ce0
Shubha <rupelpadhy@gmail.com>
2021-07-25 17:36:46 +0530

    movie verification fetching etc is being done from server side now

--- games/movie/public/static/scripts/app.js
+++ games/movie/public/static/scripts/app.js
@@ -1,52 +1,64 @@
+
 const socket = io()
-
-
-// message from server
-socket.on('message' , message =>{
-    console.log("yellow " +  message)
-    outputmsg(message)
-    // verifyMovie(message)
+const {username , room} = Qs.parse(location.search, {
+    ignoreQueryPrefix:true
 })
-
-socket.on('verify_movie' , message =>{
-    console.log("vmovie " +  message)
-    outputmsg(message)
-    verifyMovie(message)
-})
-
-// puts msg in the DOM
-function outputmsg(msg){
-    console.log(msg)
-    const div = document.createElement('div');
-    div.classList.add('message');
-    div.innerHTML = `<p class="meta">rupel <span>9:12pm</span></p>
-    <p class="text">
-    ${msg}
-    </p>`;
-
-    console.log("div : " + div)
-    document.getElementById('dis_msg').appendChild(div);
-}
-
-
-
 
 var movie
 var res 
 var list
 var c = 0
-var img
+var img 
+
+
+// message from server
+socket.on('message' , message =>{
+    outputmsg(message)
+    // verifyMovie(message)
+})
+
+// message from server that contains the movie name 
+socket.on('verify_movie' , message =>{
+    console.log("vmovie " +  message)
+    outputmsg(message)
+})
+
+
+// function correctAns(){
+//     outputmsg("correct")
+//     socket.emit('correctness',"correct")
+
+// }
+
+// function wrongAns(){
+//     socket.emit('correctness',"wrong")
+// }
+
+// puts msg in the DOM
+function outputmsg(msg){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${msg.username}<span>${msg.time}</span></p>
+    <p class="text">
+    ${msg.text}
+    </p>`;
+    document.getElementById('dis_msg').appendChild(div);
+}
+
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
-  fetch('https://api.themoviedb.org/3/discover/movie?api_key=56c5269c916c25edf98a04f984705750&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
-  .then(response => response.json())
-  .then(data =>{
-      list = data
-      img_no(c++)
-  })
+    img = document.createElement('img'); 
+    img.src = '../res/tbz logo.png'
+    document.getElementById('image_id').appendChild(img);
+//   fetch('https://api.themoviedb.org/3/discover/movie?api_key=56c5269c916c25edf98a04f984705750&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
+//   .then(response => response.json())
+//   .then(data =>{
+//       list = data
+//       img_no(c++)  
+//   })
 
   
 
@@ -64,6 +76,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
     });
 
+    socket.on('getImage', m=>{
+        movie = m
+        loadImg(movie)
+    })
+
 
 });
 
@@ -75,26 +92,13 @@ function sbtn_fun(){
     socket.emit('inputmovie', umovie)
 }
 
-function verifyMovie(umovie){
-    if(movie.original_title == umovie){
-        alert("Correct")
-        document.getElementById('image_id').removeChild(img);
-        img_no(c++)
-    }
-    else{
-        alert("not at all Correct")
-    }
-}
 
+// function img_no(i){
+//     movie = list.results[i] 
+//     load_img(movie)
+// }
 
-function img_no(i){
-    console.log("hu")
-    console.log(list)
-    movie = list.results[i] 
-    load_img(movie)
-}
-
-function load_img(m){
+function loadImg(m){
         // getting the poster and movie name using themoviedb api
     fetch('https://api.themoviedb.org/3/search/movie?api_key=56c5269c916c25edf98a04f984705750&query=' + m.original_title)
   .then(response => response.json())
@@ -103,9 +107,9 @@ function load_img(m){
       res = data
       var pp = data.results[0].poster_path 
       var name = data.results[0].original_title
+      document.getElementById('image_id').removeChild(img)
       img = document.createElement('img'); 
       img.src = 'https://image.tmdb.org/t/p/w500'+pp; 
       document.getElementById('image_id').appendChild(img);
-      console.log(pp + " " + name)
   });
 }
--- games/movie/server/server.js
+++ games/movie/server/server.js
@@ -2,26 +2,73 @@ const path = require('path')
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+const fetch = require('node-fetch');
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+var listOfMovies
+var movie
 
+var count = Math.floor(Math.random() * 11)
 // set static folder
 app.use(express.static(path.join(__dirname,'../public')))
 
+fetch('https://api.themoviedb.org/3/discover/movie?api_key=56c5269c916c25edf98a04f984705750&language=en-US&sort_by=revenue.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate')
+.then(response => response.json())
+.then(data =>{
+    listOfMovies = data
+    img_no(count , listOfMovies)
+})
+
+function img_no(i, list){
+    movie = list.results[i] 
+}
+
+function verifyMovie(umovie){
+    console.log(movie)
+    if(movie.title.toString().toLowerCase() == umovie.toString().toLowerCase() ){
+        var temp = count 
+        while(count == temp){
+            count = Math.floor(Math.random() * 11)
+            console.log(count)
+        }
+        img_no(count, listOfMovies)
+        return "correct"
+
+    }
+    else{
+        return "wrong"
+    }
+}
+
 // run when client connects
 io.on('connection', socket=>{
-    console.log("connected yoo : " + socket.id )
-    socket.emit('message' , 'this is a message')
+    socket.emit('message' , formatMessage('timelessBOT' , 'welcome everyone'))
 
     socket.on('inputmovie', umovie =>{
         console.log(umovie)
-        io.emit('verify_movie' , umovie)
+        var cor = verifyMovie(umovie)
+        io.emit('verify_movie' ,formatMessage('USER' , umovie ) )
+        io.emit('message', formatMessage('timelessBOT' , cor))
+        if(cor == "correct"){
+            io.emit('getImage', movie)
+        }
     })
 
+
+
+    io.emit('getImage', movie)
+
+
 })
+
+
+
+
+
 
 // port
 const PORT = 3000 || process.env.PORT
